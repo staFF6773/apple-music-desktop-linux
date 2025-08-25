cd8154dd9331b5b95ffe49c3285bf8fc88f303f7
Not_staff <108166164+staFF6773@users.noreply.github.com>
2025-08-25 16:19:46 -0600

    preload.ts / The method for saving cookies has been added.

--- src/preload.ts
+++ src/preload.ts
@@ -5,6 +5,18 @@ import { contextBridge, ipcRenderer } from 'electron';
 contextBridge.exposeInMainWorld('electronAPI', {
   getAppVersion: () => ipcRenderer.invoke('get-app-version'),
   getAppName: () => ipcRenderer.invoke('get-app-name'),
+  
+  // Cookie management methods
+  getCookies: (url: string) => ipcRenderer.invoke('get-cookies', url),
+  setCookie: (cookieDetails: Electron.CookiesSetDetails) => ipcRenderer.invoke('set-cookie', cookieDetails),
+  removeCookie: (url: string, name: string) => ipcRenderer.invoke('remove-cookie', url, name),
+  clearAllCookies: () => ipcRenderer.invoke('clear-all-cookies'),
+  getAllCookies: () => ipcRenderer.invoke('get-all-cookies'),
+  
+  // Menu event listeners
+  onCookiesCleared: (callback: () => void) => ipcRenderer.on('cookies-cleared', callback),
+  onReloadRequested: (callback: () => void) => ipcRenderer.on('reload-requested', callback),
+  
   // Add more API methods as needed
 });
 
@@ -14,6 +26,17 @@ declare global {
     electronAPI: {
       getAppVersion: () => Promise<string>;
       getAppName: () => Promise<string>;
+      
+      // Cookie management types
+      getCookies: (url: string) => Promise<Electron.Cookie[]>;
+      setCookie: (cookieDetails: Electron.CookiesSetDetails) => Promise<boolean>;
+      removeCookie: (url: string, name: string) => Promise<boolean>;
+      clearAllCookies: () => Promise<boolean>;
+      getAllCookies: () => Promise<Electron.Cookie[]>;
+      
+      // Menu event listeners
+      onCookiesCleared: (callback: () => void) => void;
+      onReloadRequested: (callback: () => void) => void;
     };
   }
 }
